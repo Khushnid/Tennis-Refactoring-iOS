6f5a7a29045b9815e2c41525b34b556fb98ffada
Khushnid <99318942+chowiskhush@users.noreply.github.com>
2022-05-17 02:47:28 +0500

    Finalized project with where clouses

--- Tennis/MyTennisGame.swift
+++ Tennis/MyTennisGame.swift
@@ -1,37 +1,34 @@
 import Foundation
 
 final class MyTennisGame: TennisGame {
-  
-    private var P1point: Int = 0
-    private var P2point: Int = 0
-
-    required init(player1 p1: String, player2 p2: String) {}
+    private var p1Pts: Int = 0
+    private var p2Pts: Int = 0
+    
+    init(player1: String, player2: String) {}
     
     func wonPoint(_ playerName: String) {
-        playerName == "player1" ? (P1point += 1) : (P2point += 1)
+        playerName == "player1" ? (p1Pts += 1) : (p2Pts += 1)
     }
     
     var score: String? {
-        getScore(p1Pts: P1point, p2Pts: P2point)
-    }
-}
-
-extension MyTennisGame {
-    func getScore(p1Pts: Int, p2Pts: Int) -> String {
         switch true {
-            case p1Pts >= 4 && (p1Pts - p2Pts) >= 2: return "Win for player1"
-            case p2Pts >= 4 && (p2Pts - p1Pts) >= 2: return "Win for player2"
-            case p1Pts > p2Pts && p2Pts >= 3: return "Advantage player1"
-            case p2Pts > p1Pts && p1Pts >= 3: return "Advantage player2"
-            case p1Pts == p2Pts && p1Pts > 2: return "Deuce"
-            case p1Pts == p2Pts: return currentScore(r1: p1Pts, r2: nil)
-            case p1Pts > 0 && p2Pts == 0: return currentScore(r1: p1Pts)
-            case p2Pts > 0 && p1Pts == 0: return currentScore(r2: p2Pts)
-            default: return currentScore(r1: p1Pts, r2: p2Pts)
+            case p1Pts - p2Pts >= 2 where p1Pts > 3: return "Win for player1"
+            case p2Pts - p1Pts >= 2 where p2Pts > 3: return "Win for player2"
+            case p1Pts > p2Pts where p2Pts > 2: return "Advantage player1"
+            case p2Pts > p1Pts where p1Pts > 2: return "Advantage player2"
+            case p1Pts == p2Pts where p1Pts > 2: return "Deuce"
+            case p1Pts > 0 where p2Pts == 0: return currentScore(p1Res: p1Pts)
+            case p2Pts > 0 where p1Pts == 0: return currentScore(p2Res: p2Pts)
+            case p1Pts == p2Pts: return currentScore(p1Res: p1Pts, p2Res: nil)
+            default: return currentScore(p1Res: p1Pts, p2Res: p2Pts)
         }
     }
     
-    func getScore(for score: Int) -> String {
+    private func currentScore(p1Res: Int = 0, p2Res: Int? = 0) -> String {
+        return "\(getScore(for: p1Res))-\(getScore(for: p2Res ?? -1))"
+    }
+    
+    private func getScore(for score: Int) -> String {
         switch score {
             case 0: return "Love"
             case 1: return "Fifteen"
@@ -39,9 +36,5 @@ extension MyTennisGame {
             case 3: return "Forty"
             default: return "All"
         }
-    }
-    
-    private func currentScore(r1: Int = 0, r2: Int? = 0) -> String {
-        "\(getScore(for: r1))-\(getScore(for: r2 ?? -1))"
     }
 }
--- Tennis/TennisGame.swift
+++ Tennis/TennisGame.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol TennisGame {
+public protocol TennisGame {
     init(player1: String, player2: String)
     func wonPoint(_ playerName: String)
     var score: String? { get }
